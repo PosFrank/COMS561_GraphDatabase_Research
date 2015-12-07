8dfa7c34470ba5fb3f86d42e3d1fb3ad66a842b4
FrankisFine <zjcxgaoxing@gmail.com>
2015-12-06 23:23:00 -0600

    Correct java generate files added

--- Data Generate Code/Diff_Person_Number/Create1000.java
+++ Data Generate Code/Diff_Person_Number/Create1000.java
@@ -1,13 +1,11 @@
 import java.io.*;
 
+import com.sun.org.apache.xpath.internal.operations.And;
+
 public class Create1000 {
-    
-    private static final int PERSON_NUMBER = 1000;
-    
 	public static void main(String[] args) throws IOException {
-		FileOutputStream fout = new FileOutputStream(
-				new File("C:\\Users\\as\\origindata.txt"));
-		PrintStream p = new PrintStream(fout);//
+		final int numberOfPeople = 1000;
+		
 		// p.println to write into txt file.
 		String[] firstNameSet = new String[10];
 		String[] lastNameSet = new String[10];
@@ -34,105 +32,131 @@ public class Create1000 {
 		lastNameSet[8] = "Poppy";
 		lastNameSet[9] = "Randy";
 		///////////////////////
+		String URL = "C:\\Users\\yingw\\Desktop\\561\\Project\\";
+		FileOutputStream fout = new FileOutputStream(
+				new File(URL+"origindata.txt"));
+		PrintStream p = new PrintStream(fout);//
+		
 		FileOutputStream fout1 = new FileOutputStream(
-				new File("C:\\Users\\as\\dataOfNodeGender.txt"));
+				new File(URL+"dataOfNodeGender.txt"));
 		PrintStream p1 = new PrintStream(fout1);// In p1 I use gender as a node
 		
-		////////////////
-		
-
 		// create age node
 		FileOutputStream fout2 = new FileOutputStream(
-				new File("C:\\Users\\as\\dataOfNodeAge.txt"));
+				new File(URL+"dataOfNodeAge.txt"));
 		PrintStream p2 = new PrintStream(fout2);
-		String ageNodes = "";
 		
 		
 		// create age and gender
 		FileOutputStream fout3 = new FileOutputStream(
-				new File("C:\\Users\\as\\dataOfNodeAandG.txt"));
+				new File(URL+"dataOfNodeAandG.txt"));
 		PrintStream p3 = new PrintStream(fout3);
 		
 		//create age above gender
 		FileOutputStream fout4 = new FileOutputStream(
-				new File("C:\\Users\\as\\dataOfNodeAgeAboveGender.txt"));
+				new File(URL+"dataOfNodeAgeAboveGender.txt"));
 		PrintStream p4 = new PrintStream(fout4);
 		
 		//create gender above age
 		FileOutputStream fout5 = new FileOutputStream(
-				new File("C:\\Users\\as\\dataOfNodeGenderAboveAge.txt"));
+				new File(URL+"dataOfNodeGenderAboveAge.txt"));
 		PrintStream p5 = new PrintStream(fout5);
 
+		String ageNodes = "";
+		
 		for (int i = 0; i < 100; i++) {
 			ageNodes = "create (A" + (i) + ":Age {age:" + (i) + "});";
+			
 			p2.println(ageNodes);
 			p3.println(ageNodes);
 			p4.println(ageNodes);
-			p5.println(ageNodes);
+			//p5.println(ageNodes);
+			String ageGender = "create (A"+ (i) +"Male:Gender {name:'Male',age:"+ (i) +"});";
+			p4.println(ageGender);
+			ageGender = "create (A"+ (i) +"Female:Gender {name:'Female',age:"+ (i) +"});";
+			p4.println(ageGender);
+			String relation4 = "match (c:Age), (d:Gender) where c.age = "+i+" and d.name='Male' and c.age=d.age create (c)-[r:AgeToGender]->(d);";
+			p4.println(relation4);
+			relation4 = "match (c:Age), (d:Gender) where c.age = "+i+" and d.name='Female' and c.age=d.age create (c)-[r:AgeToGender]->(d);";
+			p4.println(relation4);
+			
+			
+			
 		}
 		
 		// create gender node
-		String genderNodes = "create (Male:Gender {name:'Male'})";
+		String genderNodes = "create (Male:Gender {name:'Male'});";
 		p1.println(genderNodes);
 		p3.println(genderNodes);
-		p4.println(genderNodes);
+		//p4.println(genderNodes);
 		p5.println(genderNodes);
-		genderNodes = "create (Female:Gender {name:'Female'})";
+		genderNodes = "create (Female:Gender {name:'Female'});";
 		p1.println(genderNodes);
 		p3.println(genderNodes);
-		p4.println(genderNodes);
+		//p4.println(genderNodes);
 		p5.println(genderNodes);
-		for (int i = 0; i < PERSON_NUMBER; i++) {
+		
+		for(int j = 0; j <100;j++) {
+			String genderAge = "create (A"+ (j) +"Male:Age {name:'Male',age:"+ (j) +"});";
+			p5.println(genderAge);
+			genderAge = "create (A"+ (j) +"Female:Age {name:'Female',age:"+ (j) +"});";
+			p5.println(genderAge);
+			String relation5 = "match (c:Age), (d:Gender) where c.age = "+j+" and d.name='" +"Male"+"' and c.name=d.name create (d)-[r:GenderToAge]->(c);";
+			p5.println(relation5);
+			relation5 = "match (c:Age), (d:Gender) where c.age = "+j+" and d.name='" +"Female"+"' and c.name=d.name create (d)-[r:GenderToAge]->(c)";
+			p5.println(relation5);
+		}
+		/*
+		 * this for loop generate all the related neo4j node create query
+		 */
+		for (int i = 0; i <numberOfPeople; i++) {
 			
 			String firstname = firstNameSet[i % 10];
 			String lastname = lastNameSet[(int) (100 * Math.random()) % 10];
 			int age = (int) (99 * Math.random());
 			double genderChoose = Math.random();
 			String gender = (genderChoose > 0.5 ? "Male" : "Female");
-            
-			String query = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "',age:" + age
-					+ ",gender:'" + gender + "'})";
+			String query = "create (P" + (i + 1) + ":Person {ID"+(i+1)+", name:'" + firstname + " " + lastname + "',age:" + age
+					+ ",gender:'" + gender + "'});";
 			p.println(query);
 			
 			//System.out.println(firstname + " "+lastname + " "+ gender + " "+ age);
 			
 			// create relation
-			String query1 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "',age:" + age
-					+ "})";
+			String query1 = "create (P" + (i + 1) + ":Person{ID: "+(i+1) +", name:'" + firstname + " " + lastname + "',age:" + age
+					+ "});";
 			p1.println(query1);
-			String relation1 = "create (" + gender + ":Gender)-[:Has_Gender]->(P" + (i + 1) + ")";
+			String relation1 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
 			p1.println(relation1);
 			
-			
-			String query2 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "',gender:'"
+			String query2 = "create (P" + (i + 1) + ":Person {ID: "+(i+1) +",name:'" + firstname + " " + lastname + "',gender:'"
 					+ gender + "'});";
 			p2.println(query2);
-			String relation2 = "create (A" + age + ":Age)-[:Has_Age]->(P" + (i + 1) + ");";
+			String relation2 = "match (a:Age),(b:Person) where a.age= "+age+ " and b.ID="+(i+1)+" create (a)-[r:Has_Age]->(b);";
 			p2.println(relation2);
 			
 			//age and gender
-			String query3 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "'})";
+			String query3 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
 			p3.println(query3);
-			String relation3 = "create (A" + age + ")-[:Has_Age]->(P" + (i + 1) + ")";
+			String relation3 = "match (a:Age),(b:Person) where a.age =" + age + " and b.ID ="+(i+1)+" create (a)-[r:Has_Age]->(b);";
 			p3.println(relation3);
-			relation3 = "create (" + gender + ")-[:Has_Gender]->(P" + (i + 1) + ")";
+			relation3 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
 			p3.println(relation3);
 			
 			//age above gender
-			String query4 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "'})";
+			String query4 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
 			p4.println(query4);
-			String relation4 = "create (" + gender + ")-[:Has_Gender]->(P" + (i + 1) + ")";
+			String relation4 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and a.age="+ age +" and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
 			p4.println(relation4);
-			relation4 = "create (A" + age + ")-[:AgeToGender]->(" + gender + ")";
-			p4.println(relation4);
+					
+		
 			
 			//gender above age
-			String query5 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "'})";
+			String query5 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
 			p5.println(query5);
-			String relation5 = "create (A" + age + ")-[:Has_Age]->(P" + (i + 1) + ")";
+			String relation5 = "match (a:Age),(b:Person) where a.age =" + age + " and a.name='"+gender+"' and b.ID ="+(i+1)+" create (a)-[r:Has_Age]->(b);";
 			p5.println(relation5);
-			relation5 = "create (" + gender + ")-[:GenderToAge]->(A" + age + ")";
-			p5.println(relation5);
+			
 			
 		}
 		p.close();
---
+++ Data Generate Code/Diff_Person_Number/Create50000.java
@@ -0,0 +1,169 @@
+import java.io.*;
+
+import com.sun.org.apache.xpath.internal.operations.And;
+
+public class Create50000 {
+	public static void main(String[] args) throws IOException {
+		final int numberOfPeople = 50000;
+		
+		// p.println to write into txt file.
+		String[] firstNameSet = new String[10];
+		String[] lastNameSet = new String[10];
+		// int age;
+		firstNameSet[0] = "Zoey";
+		firstNameSet[1] = "Barney";
+		firstNameSet[2] = "Robin";
+		firstNameSet[3] = "Alex";
+		firstNameSet[4] = "Ted";
+		firstNameSet[5] = "Harry";
+		firstNameSet[6] = "Jamie";
+		firstNameSet[7] = "Kevin";
+		firstNameSet[8] = "Marvin";
+		firstNameSet[9] = "Nancy";
+		//////////////////////
+		lastNameSet[0] = "Mosby";
+		lastNameSet[1] = "Aldrin";
+		lastNameSet[2] = "Case";
+		lastNameSet[3] = "Wang";
+		lastNameSet[4] = "James";
+		lastNameSet[5] = "Donald";
+		lastNameSet[6] = "Eric";
+		lastNameSet[7] = "Stinson";
+		lastNameSet[8] = "Poppy";
+		lastNameSet[9] = "Randy";
+		///////////////////////
+		String URL = "C:\\Users\\yingw\\Desktop\\561\\Project\\";
+		FileOutputStream fout = new FileOutputStream(
+				new File(URL+"origindata.txt"));
+		PrintStream p = new PrintStream(fout);//
+		
+		FileOutputStream fout1 = new FileOutputStream(
+				new File(URL+"dataOfNodeGender.txt"));
+		PrintStream p1 = new PrintStream(fout1);// In p1 I use gender as a node
+		
+		// create age node
+		FileOutputStream fout2 = new FileOutputStream(
+				new File(URL+"dataOfNodeAge.txt"));
+		PrintStream p2 = new PrintStream(fout2);
+		
+		
+		// create age and gender
+		FileOutputStream fout3 = new FileOutputStream(
+				new File(URL+"dataOfNodeAandG.txt"));
+		PrintStream p3 = new PrintStream(fout3);
+		
+		//create age above gender
+		FileOutputStream fout4 = new FileOutputStream(
+				new File(URL+"dataOfNodeAgeAboveGender.txt"));
+		PrintStream p4 = new PrintStream(fout4);
+		
+		//create gender above age
+		FileOutputStream fout5 = new FileOutputStream(
+				new File(URL+"dataOfNodeGenderAboveAge.txt"));
+		PrintStream p5 = new PrintStream(fout5);
+
+		String ageNodes = "";
+		
+		for (int i = 0; i < 100; i++) {
+			ageNodes = "create (A" + (i) + ":Age {age:" + (i) + "});";
+			
+			p2.println(ageNodes);
+			p3.println(ageNodes);
+			p4.println(ageNodes);
+			//p5.println(ageNodes);
+			String ageGender = "create (A"+ (i) +"Male:Gender {name:'Male',age:"+ (i) +"});";
+			p4.println(ageGender);
+			ageGender = "create (A"+ (i) +"Female:Gender {name:'Female',age:"+ (i) +"});";
+			p4.println(ageGender);
+			String relation4 = "match (c:Age), (d:Gender) where c.age = "+i+" and d.name='Male' and c.age=d.age create (c)-[r:AgeToGender]->(d);";
+			p4.println(relation4);
+			relation4 = "match (c:Age), (d:Gender) where c.age = "+i+" and d.name='Female' and c.age=d.age create (c)-[r:AgeToGender]->(d);";
+			p4.println(relation4);
+			
+			
+			
+		}
+		
+		// create gender node
+		String genderNodes = "create (Male:Gender {name:'Male'});";
+		p1.println(genderNodes);
+		p3.println(genderNodes);
+		//p4.println(genderNodes);
+		p5.println(genderNodes);
+		genderNodes = "create (Female:Gender {name:'Female'});";
+		p1.println(genderNodes);
+		p3.println(genderNodes);
+		//p4.println(genderNodes);
+		p5.println(genderNodes);
+		
+		for(int j = 0; j <100;j++) {
+			String genderAge = "create (A"+ (j) +"Male:Age {name:'Male',age:"+ (j) +"});";
+			p5.println(genderAge);
+			genderAge = "create (A"+ (j) +"Female:Age {name:'Female',age:"+ (j) +"});";
+			p5.println(genderAge);
+			String relation5 = "match (c:Age), (d:Gender) where c.age = "+j+" and d.name='" +"Male"+"' and c.name=d.name create (d)-[r:GenderToAge]->(c);";
+			p5.println(relation5);
+			relation5 = "match (c:Age), (d:Gender) where c.age = "+j+" and d.name='" +"Female"+"' and c.name=d.name create (d)-[r:GenderToAge]->(c)";
+			p5.println(relation5);
+		}
+		/*
+		 * this for loop generate all the related neo4j node create query
+		 */
+		for (int i = 0; i <numberOfPeople; i++) {
+			
+			String firstname = firstNameSet[i % 10];
+			String lastname = lastNameSet[(int) (100 * Math.random()) % 10];
+			int age = (int) (99 * Math.random());
+			double genderChoose = Math.random();
+			String gender = (genderChoose > 0.5 ? "Male" : "Female");
+			String query = "create (P" + (i + 1) + ":Person {ID"+(i+1)+", name:'" + firstname + " " + lastname + "',age:" + age
+					+ ",gender:'" + gender + "'});";
+			p.println(query);
+			
+			//System.out.println(firstname + " "+lastname + " "+ gender + " "+ age);
+			
+			// create relation
+			String query1 = "create (P" + (i + 1) + ":Person{ID: "+(i+1) +", name:'" + firstname + " " + lastname + "',age:" + age
+					+ "});";
+			p1.println(query1);
+			String relation1 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
+			p1.println(relation1);
+			
+			String query2 = "create (P" + (i + 1) + ":Person {ID: "+(i+1) +",name:'" + firstname + " " + lastname + "',gender:'"
+					+ gender + "'});";
+			p2.println(query2);
+			String relation2 = "match (a:Age),(b:Person) where a.age= "+age+ " and b.ID="+(i+1)+" create (a)-[r:Has_Age]->(b);";
+			p2.println(relation2);
+			
+			//age and gender
+			String query3 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
+			p3.println(query3);
+			String relation3 = "match (a:Age),(b:Person) where a.age =" + age + " and b.ID ="+(i+1)+" create (a)-[r:Has_Age]->(b);";
+			p3.println(relation3);
+			relation3 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
+			p3.println(relation3);
+			
+			//age above gender
+			String query4 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
+			p4.println(query4);
+			String relation4 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and a.age="+ age +" and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
+			p4.println(relation4);
+					
+		
+			
+			//gender above age
+			String query5 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
+			p5.println(query5);
+			String relation5 = "match (a:Age),(b:Person) where a.age =" + age + " and a.name='"+gender+"' and b.ID ="+(i+1)+" create (a)-[r:Has_Age]->(b);";
+			p5.println(relation5);
+			
+			
+		}
+		p.close();
+		p1.close();
+		p2.close();
+		p3.close();
+		p4.close();
+		p5.close();
+	}
+}
