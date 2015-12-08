6f650f81fb33eb371ed01211cdcb375c6bdc2bb2
FrankisFine <FrankisFine@users.noreply.github.com>
2015-12-07 21:09:52 -0600

    match to create

--- Data Generate Code/Diff_Person_Number/Create1000.java
+++ Data Generate Code/Diff_Person_Number/Create1000.java
@@ -79,12 +79,24 @@ public class Create1000 {
 			p4.println(relation4);
 			relation4 = "match (c:Age), (d:Gender) where c.age = "+i+" and d.name='Female' and c.age=d.age create (c)-[r:AgeToGender]->(d);";
 			p4.println(relation4);
-			
-			
-			
 		}
-		
-		// create gender node
+		for (int i = 0; i < 100; i++) {
+			ageNodes = "create (A" + (i) + ":Age {age:" + (i) + "});";
+			
+			p2.println(ageNodes);
+			p3.println(ageNodes);
+			p4.println(ageNodes);
+			//p5.println(ageNodes);
+			String ageGender = "create (A"+ (i) +"Male:Gender {name:'Male',age:"+ (i) +"});";
+			p4.println(ageGender);
+			ageGender = "create (A"+ (i) +"Female:Gender {name:'Female',age:"+ (i) +"});";
+			p4.println(ageGender);
+			String relation4 = "create ((A"+(i)+")-[r:AgeToGender]->(A"+(i)+"Male);)";
+			p4.println(relation4);
+			relation4 = "create ((A"+(i)+")-[r:AgeToGender]->(A"+(i)+"Female);)";
+			p4.println(relation4);
+		}
+
 		String genderNodes = "create (Male:Gender {name:'Male'});";
 		p1.println(genderNodes);
 		p3.println(genderNodes);
@@ -101,11 +113,12 @@ public class Create1000 {
 			p5.println(genderAge);
 			genderAge = "create (A"+ (j) +"Female:Age {name:'Female',age:"+ (j) +"});";
 			p5.println(genderAge);
-			String relation5 = "match (c:Age), (d:Gender) where c.age = "+j+" and d.name='" +"Male"+"' and c.name=d.name create (d)-[r:GenderToAge]->(c);";
+			String relation5 = "create ((A"+(j)+"Male)-[r:AgeToGender]->(A"+(j)+");)";
 			p5.println(relation5);
-			relation5 = "match (c:Age), (d:Gender) where c.age = "+j+" and d.name='" +"Female"+"' and c.name=d.name create (d)-[r:GenderToAge]->(c)";
+			relation5 = "create ((A"+(j)+"Female)-[r:AgeToGender]->(A"+(j)+");)";
 			p5.println(relation5);
 		}
+
 		/*
 		 * this for loop generate all the related neo4j node create query
 		 */
@@ -123,38 +136,37 @@ public class Create1000 {
 			//System.out.println(firstname + " "+lastname + " "+ gender + " "+ age);
 			
 			// create relation
-			String query1 = "create (P" + (i + 1) + ":Person{ID: "+(i+1) +", name:'" + firstname + " " + lastname + "',age:" + age
-					+ "});";
+			String query1 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "',age:" + age
+					+ "})";
 			p1.println(query1);
-			String relation1 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
+			String relation1 = "create (" + gender + ")-[:Has_Gender]->(P" + (i + 1) + ")";
 			p1.println(relation1);
 			
-			String query2 = "create (P" + (i + 1) + ":Person {ID: "+(i+1) +",name:'" + firstname + " " + lastname + "',gender:'"
+			
+			String query2 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "',gender:'"
 					+ gender + "'});";
 			p2.println(query2);
-			String relation2 = "match (a:Age),(b:Person) where a.age= "+age+ " and b.ID="+(i+1)+" create (a)-[r:Has_Age]->(b);";
+			String relation2 = "create (A" + age + ")-[:Has_Age]->(P" + (i + 1) + ");";
 			p2.println(relation2);
 			
 			//age and gender
-			String query3 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
+			String query3 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "'})";
 			p3.println(query3);
-			String relation3 = "match (a:Age),(b:Person) where a.age =" + age + " and b.ID ="+(i+1)+" create (a)-[r:Has_Age]->(b);";
+			String relation3 = "create (A" + age + ")-[:Has_Age]->(P" + (i + 1) + ")";
 			p3.println(relation3);
-			relation3 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
+			relation3 = "create (" + gender + ")-[:Has_Gender]->(P" + (i + 1) + ")";
 			p3.println(relation3);
 			
 			//age above gender
-			String query4 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
+			String query4 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "'})";
 			p4.println(query4);
-			String relation4 = "match (a:Gender),(b:Person) where a.name='"+gender+"' and a.age="+ age +" and b.ID="+(i+1)+" create (a)-[r:Has_Gender]->(b);";
+			String relation4 = "create (A"+age + gender + ")-[:Has_Gender]->(P" + (i + 1) + ")";
 			p4.println(relation4);
-					
-		
 			
 			//gender above age
-			String query5 = "create (P" + (i + 1) + ":Person {ID: "+(i+1)+",name:'" + firstname + " " + lastname + "'});";
+			String query5 = "create (P" + (i + 1) + ":Person {name:'" + firstname + " " + lastname + "'})";
 			p5.println(query5);
-			String relation5 = "match (a:Age),(b:Person) where a.age =" + age + " and a.name='"+gender+"' and b.ID ="+(i+1)+" create (a)-[r:Has_Age]->(b);";
+			String relation5 = "create (A" + age + gender+")-[:Has_Age]->(P" + (i + 1) + ")";
 			p5.println(relation5);
 			
 			
